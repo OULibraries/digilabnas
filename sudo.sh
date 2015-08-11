8b09343c1223f9933f6ad898acef60642d623971
libacct <libacct@lib-52.sooner.net.ou.edu>
2015-08-11 11:48:45 -0500

    fixed typo in variable name

--- sudo.sh
+++ sudo.sh
@@ -5,7 +5,7 @@ echo ""
 echo " $* "
 echo ""
 
-read -p  "Are you sure that you're not being dumb? (y/n)" $CONFIRM
+read -p  "Are you sure that you're not being dumb? (y/n)" CONFIRM
 
 if [ "$CONFIRM" == "y" ]
 then
